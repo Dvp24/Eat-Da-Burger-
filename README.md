0746f80c5942cf0dc04315ff6fb1d6468ecf603f
Dhanashree <dvphalak@gmail.com>
2018-11-09 13:29:23 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Eat-Da-Burger-
+DONE
+1 create db schema and seeds
+server.js
+connection.js
+html /handlebars
+
+
+
+TO-Do
+orm.js
+models/burger.js file
+controller/burgerscontroller file
+burger.js  file with ajax requests lies in assets
+apiroutes get post devour(delete)
+htmlroutes
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+INSERT INTO burgers (Burger_name) VALUES ('Chili Cheese burger');
+INSERT INTO burgers (Burger_name) VALUES ('Black Bean burger');
+INSERT INTO burgers (Burger_name, devoured) VALUES ('Veggie burger', true);
+INSERT INTO burgers (Burger_name, devoured) VALUES ('Beef burger', true);
+INSERT INTO burgers (Burger_name, devoured) VALUES ('Turkey burger', true);
+INSERT INTO burgers (Burger_name) VALUES ('Wild Salmon burger');
